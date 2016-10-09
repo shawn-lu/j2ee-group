97fcdf9d6a9931b0a6bf5f6b999ce31c130678d9
luxufeng <luxufeng@edata.com.cn>
2016-10-09 17:58:28 +0800

    init project

---
+++ ssm-demo/src/main/java/com/shawn/ssm/system/dao/UserDao.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:ssm-demo
+ * File Name:UserDao.java
+ * Package Name:com.shawn.ssm.system.dao
+ * Date:2016年10月3日下午3:18:04
+ *
+ */
+
+
+package com.shawn.ssm.system.dao;
+
+/**
+ * ClassName: UserDao <br/>
+ * Date: 2016年10月3日 下午3:18:04 <br/>
+ * Description: TODO 
+ *
+ * @author lxf
+ * @version 
+ * @see
+ */
+public interface UserDao {
+
+}
+
---
+++ ssm-demo/src/main/java/com/shawn/ssm/system/dao/impl/UserDaoImpl.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:ssm-demo
+ * File Name:UserDaoImpl.java
+ * Package Name:com.shawn.ssm.system.dao.impl
+ * Date:2016年10月3日下午3:18:48
+ *
+ */
+
+package com.shawn.ssm.system.dao.impl;
+
+import com.shawn.ssm.system.dao.UserDao;
+
+/**
+ * ClassName: UserDaoImpl <br/>
+ * Date: 2016年10月3日 下午3:18:48 <br/>
+ * Description: TODO 
+ *
+ * @author lxf
+ * @version 
+ * @see
+ */
+
+public class UserDaoImpl implements UserDao     {
+
+}
+
